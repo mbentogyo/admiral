ad1eed49b8dc1a5d8b02a523c7452e5619be1d0e
Gyo <mbentogyo@gmail.com>
2025-11-14 10:48:34 +0800

    a

--- Game/core/src/main/java/com/ThePod/Admirals/Main.java
+++ Game/core/src/main/java/com/ThePod/Admirals/Main.java
@@ -1,31 +1,33 @@
 package com.ThePod.Admirals;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx; 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ThePod.Admirals.ScreenCamera;
-import com.ThePod.Admirals.GameManager; 
+import com.ThePod.Admirals.GameManager;
+import lombok.Getter;
 
 // {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
 public class Main extends Game {
 
     // Universal Enablers To Get View Port & Sprites
+    @Getter private Main instance;
     public SpriteBatch batch;
     public ScreenCamera screenCamera;
-    public CursorHandler cursorHandler; 
+    public CursorHandler cursorHandler;
 
     @Override
     public void create() {
-    
+
         batch = new SpriteBatch();
         screenCamera = new ScreenCamera();
-        
+
         // Initialize and load assets
         AssetLoader.getInstance().loadAssets();
         AssetLoader.getInstance().finishLoading();
 
         // Assign loaded assets to the easy-access fields in AssetLoader
-        AssetLoader.getInstance().cacheAssets(); 
+        AssetLoader.getInstance().cacheAssets();
 
         // Set up the custom cursor
         // This only needs to be done once
@@ -34,6 +36,7 @@ public class Main extends Game {
 
         // Sets The First Screen As Game Opens
         setScreen(new MainMenu(this));
+        instance = this;
     }
 
     @Override
