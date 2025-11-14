606f8f6b6165f124e8d0d81aff9faae4ba3de6a1
Gyo <mbentogyo@gmail.com>
2025-11-14 11:04:55 +0800

    a

--- Game/core/src/main/java/com/ThePod/Admirals/PlayScreen.java
+++ Game/core/src/main/java/com/ThePod/Admirals/PlayScreen.java
@@ -48,6 +48,14 @@ public class PlayScreen implements Screen {
         this.stage = new Stage(this.viewport, game.batch);
         instance = this;
 
+        Gdx.input.setInputProcessor(stage);
+        this.atlas = AssetLoader.getInstance().admiralsUiAtlas;
+        this.font = AssetLoader.getInstance().operatorFont;
+        this.background = AssetLoader.getInstance().getTexture("Play_Frame.png");
+
+        // --- Create the two boards ---
+        // We pass 'this' (the PlayScreen) to the boards so they can call back
+
         // 1. Create My Board (Player)
         // FIX: Added 'this' as the second argument
         myBoard = new GameBoard(atlas, this, true); // true = isMyBoard
@@ -65,13 +73,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(stage);
-        this.atlas = AssetLoader.getInstance().admiralsUiAtlas;
-        this.font = AssetLoader.getInstance().operatorFont;
-        this.background = AssetLoader.getInstance().getTexture("Play_Frame.png");
 
-        // --- Create the two boards ---
-        // We pass 'this' (the PlayScreen) to the boards so they can call back
     }
 
     @Override
